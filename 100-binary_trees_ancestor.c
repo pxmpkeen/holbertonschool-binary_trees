6b37ff8c3d651cbefed3dcc9a8b3fadd792523a8
pxmpkeen <pxmpkeen@gmail.com>
2024-01-18 03:23:16 -0800

    inshallah ishleye

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+/**
+ * binary_trees_ancestor - finds lowest ancestor of two nodes
+ * @first: first node
+ * @second: second node
+ *
+ * Return: node or NULL
+ */
+binary_tree_t *binary_trees_ancestor(
+		const binary_tree_t *first,
+		const binary_tree_t *second
+		)
+{
+	binary_tree_t *node, *temp, *help;
+
+	node = (binary_tree_t *)second;
+	temp = (binary_tree_t *)first;
+
+	while (temp != NULL)
+	{
+		help = node;
+		while (help != NULL)
+		{
+			help = (*help).parent;
+			if (help == temp)
+				return (help);
+		}
+		temp = (*temp).parent;
+	}
+	return (NULL);
+}
